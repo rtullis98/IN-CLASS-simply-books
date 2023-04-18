56ddc8d9286730824f1801423b6dc80ac00d357f
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-04-18 18:16:07 -0500

    rendererd authors and books

--- pages/authors.js
+++ pages/authors.js
@@ -1,5 +1,37 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import { getAuthors } from '../api/authorData';
+import AuthorCard from '../components/AuthorCard';
 
-export default function Authors() {
-  return <div>authors here</div>;
+function Authors() {
+  const [authors, setAuthors] = useState([]);
+
+  const { user } = useAuth();
+
+  const getAllTheAuthors = () => {
+    getAuthors(user.uid).then(setAuthors);
+  };
+
+  useEffect(() => {
+    getAllTheAuthors();
+  }, []);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/author/new" passHref>
+        <Button>Add An Author</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {authors.map((author) => (
+          <AuthorCard key={author.firebaseKey} authorObject={author} onUpdate={getAllTheAuthors} />
+        ))}
+      </div>
+
+    </div>
+  );
 }
+
+export default Authors;
--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Signout from '../components/Signout';
 import User from '../components/User';
 import { useAuth } from '../utils/context/authContext';
 
 export default function Profile() {
   const { user } = useAuth();
-
   return (
     <>
-      <User userObj={user} />
-      <Signout />
+      <User userObject={user} />
     </>
   );
 }
